44e44b39d1abaa400cc51d768556a4b9431611f1
Justin Asfour <6599551+justinasfour04@users.noreply.github.com>
2023-01-23 23:36:12 -0500

    fix: Fixed the platforms not spawning as player moves up

--- Assets/Scripts/Mechanics/PlatformController.cs
+++ Assets/Scripts/Mechanics/PlatformController.cs
@@ -21,7 +21,7 @@ namespace FrogJump.Mechanics
     const int TOTAL_PLATFORMS = 15;
     const int PLATFORM_SPACING = 5;
     const float DOWN_DECELERATION = 0.05f;
-    [SerializeField] private Platform platformPrefab;
+    [SerializeField] private GameObject platformPrefab;
     [SerializeField] private Transform player;
     private GameModel model = Util.GetModel<GameModel>();
     private int platformCount = 0;
@@ -35,7 +35,7 @@ namespace FrogJump.Mechanics
 
       // Update SortedList
       SortedList updatedPlatforms = new SortedList(new PlatformComparer(), TOTAL_PLATFORMS);
-      foreach (Platform platform in platforms.Values)
+      foreach (GameObject platform in platforms.Values)
       {
         if (platform != null)
         {
@@ -58,14 +58,14 @@ namespace FrogJump.Mechanics
 
     private void RemoveOldestPlatform()
     {
-      Platform oldestPlatform = (Platform)platforms.GetByIndex(0);
+      GameObject oldestPlatform = (GameObject)platforms.GetByIndex(0);
 
       if (oldestPlatform.transform.position.y < model.CameraBounds.yMin - 2f)
       {
         platforms.RemoveAt(0);
         GameObject.Destroy(oldestPlatform);
 
-        Platform latestPlatform = (Platform)platforms.GetByIndex(platforms.Count - 1);
+        GameObject latestPlatform = (GameObject)platforms.GetByIndex(platforms.Count - 1);
         SpawnPlatform(latestPlatform.transform.position.y + UnityEngine.Random.Range(2, PLATFORM_SPACING));
       }
     }
@@ -100,9 +100,13 @@ namespace FrogJump.Mechanics
         randomXPos = UnityEngine.Random.Range(model.CameraBounds.xMin, model.CameraBounds.xMax);
       }
       var position = new Vector2(randomXPos, y);
-      Platform platform = Instantiate<Platform>(platformPrefab, position, Quaternion.identity, transform);
-      collisionByInstanceId.Add(platform.UniqueId, false);
-      platforms.Add(++platformCount, platformPrefab);
+      GameObject gameObject = Instantiate<GameObject>(platformPrefab, position, Quaternion.identity, transform);
+      Platform platform = gameObject.GetComponent<Platform>();
+      if (!collisionByInstanceId.ContainsKey(platform.UniqueId))
+      {
+        collisionByInstanceId.Add(platform.UniqueId, false);
+      }
+      platforms.Add(++platformCount, gameObject);
     }
   }
 }
